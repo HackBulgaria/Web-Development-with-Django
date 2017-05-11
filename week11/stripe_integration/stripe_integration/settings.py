514c52b44f88c69a0d0b1a172df6373006ac6e06
Pavlin Gergov <gergov.pavlin@gmail.com>
2017-05-11 20:57:07 +0300

    Add initial payments app

--- week11/stripe_integration/stripe_integration/settings.py
+++ week11/stripe_integration/stripe_integration/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'users.apps.UsersConfig',
-    'magazine.apps.MagazineConfig'
+    'magazine.apps.MagazineConfig',
+    'payments.apps.PaymentsConfig'
 ]
 
 MIDDLEWARE = [
